661bdc9e45c5e7ea3b3199b8a080861903f3a6cb
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MBP-de-Antonio.home>
2021-11-17 14:17:08 +0100

    pinted all

--- conoce-mi-barrio/src/components/Culture/Culture.jsx
+++ conoce-mi-barrio/src/components/Culture/Culture.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+ 
+ import { useState, useEffect } from 'react';
+ import axios from "axios"
 
-function Museum() {
-    return (
-        <div>
-            culture
-        </div>
-    )
-}
+ 
 
-export default Museum
+ const Restaurant = () => {
+     const BASEURL="https://backend-cordoba.vercel.app";
+     const ITEMSURL="/action/name/culture";
+     const [error,setError]=useState(null);
+     const [isLoaded,setIsLoaded]=useState(false);
+     const [items, setItems]=useState([]);
+
+     useEffect (()=>{
+         axios(BASEURL+ITEMSURL).then(
+            (res)=>{
+                setItems(res.data.data.Action);
+                console.log(items)
+                setIsLoaded(true);
+            } ,
+            (error)=>{
+                setIsLoaded(true);
+                setError(error);
+            }
+         )
+     },[]);
+     if(error){
+         return <div>Error:{error.message}</div>
+     }else if(!isLoaded){
+         return<div>Loading...</div>
+     }else{
+         return(
+            <ul>
+            {items.map((item)=>(
+              <li key={item._id}>
+               <h3>{item.name}</h3>
+                  <img src={item.image} alt={item.name}></img>
+                  <p>{item.adress}</p>
+                  <p>Horario:{item.hour}</p>
+              </li>  
+            ))}
+        </ul>
+         );
+     }
+
+ };
+
+ export default Restaurant
